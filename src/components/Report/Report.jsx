ce630b927993604372bd6c36387c6d8744086106
Matt Hurst <matt.adam.hurst@gmail.com>
2021-01-28 10:11:27 +0000

    feat(report.jsx): allow user to select author on dropdown, store in local state

--- src/components/Report/Report.jsx
+++ src/components/Report/Report.jsx
@@ -1,24 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Report = ({authors, posts}) => {
-  
-  
+  const [allAuthors, setAllAuthors] = useState([])
+  const [author, setAuthor] = useState() 
+
+  useEffect(() => {
+    setAllAuthors(authors)
+    setAuthor(authors[0])
+  }, [])
+
+  const handleChange = e => setAuthor(allAuthors[e.target.value])
+
   return (
     <div>
       <h2>Report</h2>
-      <div>
-      {authors.map(author => {
-        return <p>{author.lastName}</p>
-      })}
-      {posts.slice(20, 40).map(post => {
-        return (
-          <div>
-            <p>{post.author.lastName}</p>
-            <p>{post.createdAt}</p>
-          </div>
-        )
-      })}
-    </div>
+      <select onChange={handleChange}>
+        {authors.map((author, i) => {
+          return <option value={i} key={i+author.id}>{author.lastName}</option>
+        })}
+      </select>
     </div>
   )
 }
