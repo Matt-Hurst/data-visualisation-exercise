b1f6e4671860c029ffa210244210c9064a75a658
Matt Hurst <matt.adam.hurst@gmail.com>
2021-01-28 10:00:40 +0000

    refactor(app.js report.jsx): move graphql query up to App.js file from report.jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import './App.css';
+import { gql, useQuery } from '@apollo/client';
 import { Report } from './components/Report';
 
+import './App.css';
 function App() {
+  const GET_POSTS_AND_AUTHORS = gql`
+    {
+      allPosts(count: 1000) {
+        author {
+          id
+          lastName
+        }
+        createdAt
+      }
+      allUsers(count: 5) {
+        id
+        firstName
+        lastName
+      }
+    } 
+  `;
+
+  const { loading, error, data } = useQuery(GET_POSTS_AND_AUTHORS);
+  if(error) console.log(error)
+  if(loading) return <h1>LOADING</h1>
+  if(data) console.log(data)
+
   return (
     <div className="App">
       <h1>Data Visualisation Exercise</h1>
-      <Report />
+      {data && <Report authors={data.allUsers} posts={data.allPosts}/>}
     </div>
   );
 }
--- src/components/Report/Report.jsx
+++ src/components/Report/Report.jsx
@@ -1,35 +1,24 @@
 import React from 'react';
-import { gql, useQuery } from '@apollo/client';
-import { LineGraph } from '../LineGraph';
 
-const Report = () => {
-
-  const GET_POSTS_AND_AUTHORS = gql`
-    {
-      allPosts(count: 1000) {
-        author {
-          id
-          lastName
-        }
-        createdAt
-      }
-      allUsers(count: 5) {
-        id
-        firstName
-        lastName
-      }
-    } 
-  `;
-
-  const { loading, error, data } = useQuery(GET_POSTS_AND_AUTHORS);
-  if(error) console.log(error)
-  if(loading) return <h1>LOADING</h1>
-  if(data) console.log(data)
+const Report = ({authors, posts}) => {
+  
   
   return (
     <div>
       <h2>Report</h2>
-      {data && <LineGraph authors={data.allUsers} posts={data.allPosts}/>}
+      <div>
+      {authors.map(author => {
+        return <p>{author.lastName}</p>
+      })}
+      {posts.slice(20, 40).map(post => {
+        return (
+          <div>
+            <p>{post.author.lastName}</p>
+            <p>{post.createdAt}</p>
+          </div>
+        )
+      })}
+    </div>
     </div>
   )
 }
