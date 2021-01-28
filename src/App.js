62c6d557781751939ab8d310a6e9bb2cbe12664e
Matt Hurst <matt.adam.hurst@gmail.com>
2021-01-28 09:46:33 +0000

    feat(report component): create Report component, and set up graphql query

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import { Report } from './components/Report';
 
 function App() {
   return (
     <div className="App">
       <h1>Data Visualisation Exercise</h1>
+      <Report />
     </div>
   );
 }
---
+++ src/components/Report/Report.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { gql, useQuery } from '@apollo/client';
+
+const Report = () => {
+
+  const GET_POSTS_AND_AUTHORS = gql`
+    {
+      allPosts(count: 1000) {
+        author {
+          id
+          lastName
+        }
+        createdAt
+      }
+      allUsers(count: 5) {
+        id
+        firstName
+        lastName
+      }
+    } 
+  `;
+
+  const { loading, error, data } = useQuery(GET_POSTS_AND_AUTHORS);
+  if(error) console.log(error)
+  if(loading) return <h1>LOADING</h1>
+  if(data) console.log(data)
+  
+  return (
+    <div>
+      <h2>Report</h2>
+    </div>
+  )
+}
+
+export default Report;
